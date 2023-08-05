9b28ac87783af357d9d0393b31a68f898b1e575d
DeodatoBastos <deodatobasto@gmail.com>
2023-08-05 13:04:13 -0300

    improve readability in close buffer

--- after/plugin/close-buffers.lua
+++ after/plugin/close-buffers.lua
@@ -1,13 +1,17 @@
 require('close_buffers').setup({
-  preserve_window_layout = { 'this' },
-  next_buffer_cmd = function(windows)
-    require('bufferline').cycle(1)
-    local bufnr = vim.api.nvim_get_current_buf()
+    preserve_window_layout = { 'this' },
+    next_buffer_cmd = function(windows)
+        require('bufferline').cycle(1)
+        local bufnr = vim.api.nvim_get_current_buf()
 
-    for _, window in ipairs(windows) do
-      vim.api.nvim_win_set_buf(window, bufnr)
-    end
-  end,
+        for _, window in ipairs(windows) do
+            vim.api.nvim_win_set_buf(window, bufnr)
+        end
+    end,
 })
 
-vim.keymap.set("n", "<leader>c", "[[<CMD>lua require('close_buffers').delete({type = 'this'})<CR>]]", { noremap = true, silent = true })
+local function delete_current_buffer()
+    require("close_buffers").delete({ type = "this" })
+end
+
+vim.keymap.set({ "n", "t" }, "<leader>c", delete_current_buffer, { noremap = true, silent = true })
