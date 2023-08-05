29dd23e3132c232831d57d0e869f4fc661507b6c
DeodatoBastos <deodatobasto@gmail.com>
2023-08-04 21:01:33 -0300

    update comments configs

--- after/plugin/comment.lua
+++ after/plugin/comment.lua
@@ -1,3 +1,12 @@
-require('Comment').setup()
+require('Comment').setup({
+    ignore = "^$",
+    pre_hook = function(...)
+      local loaded, ts_comment = pcall(require, "ts_context_commentstring.integrations.comment_nvim")
+      if loaded and ts_comment then
+        return ts_comment.create_pre_hook()(...)
+      end
+    end,
+})
 
 vim.keymap.set("n", "<leader>/", "<Plug>(comment_toggle_linewise_current)")
+vim.keymap.set("v", "<leader>/", "<Plug>(comment_toggle_linewise_visual)")
