91210e6f1fe20ba89da52a8105434ab3881938c6
DeodatoBastos <deodatobasto@gmail.com>
2023-09-11 16:51:02 -0300

    update bash server settings

--- lua/settings/lsp/bashls.lua
+++ lua/settings/lsp/bashls.lua
@@ -1,8 +1,8 @@
 return {
     settings = {
         bash = {
-            cmd = { 'bash-language-server', 'start' },
-            filetypes = { "bash", "zsh" },
+            cmd = { "bash-language-server", "start" },
+            filetypes = { "bash", "zsh", "sh" },
             settings = {
                 bashIde = {
                     globPattern = "*@(.sh|.inc|.bash|.command|.zsh)"
