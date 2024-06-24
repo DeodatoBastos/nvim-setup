91c85f26778fd49ee8e75e0f03a3e1491c6e6de3
DeodatoBastos <deodatobasto@gmail.com>
2024-06-24 18:10:48 -0300

    fix: fix gitsigns config

--- lua/deodato/plugins/gitsigns.lua
+++ lua/deodato/plugins/gitsigns.lua
@@ -6,34 +6,19 @@ return {
         require("gitsigns").setup({
             signs = {
                 add = {
-                    hl = "GitSignsAdd",
                     text = icons.ui.BoldLineLeft,
-                    numhl = "GitSignsAddNr",
-                    linehl = "GitSignsAddLn",
                 },
                 change = {
-                    hl = "GitSignsChange",
                     text = icons.ui.BoldLineLeft,
-                    numhl = "GitSignsChangeNr",
-                    linehl = "GitSignsChangeLn",
                 },
                 delete = {
-                    hl = "GitSignsDelete",
                     text = icons.ui.Triangle,
-                    numhl = "GitSignsDeleteNr",
-                    linehl = "GitSignsDeleteLn",
                 },
                 topdelete = {
-                    hl = "GitSignsDelete",
                     text = icons.ui.Triangle,
-                    numhl = "GitSignsDeleteNr",
-                    linehl = "GitSignsDeleteLn",
                 },
                 changedelete = {
-                    hl = "GitSignsChange",
                     text = icons.ui.BoldLineLeft,
-                    numhl = "GitSignsChangeNr",
-                    linehl = "GitSignsChangeLn",
                 },
             },
             watch_gitdir = {
